1aa119dd2bb74ade4c00d0a716ccb15d9b31afde
HusterCxh <cxh@hust.edu.cn>
2018-12-26 23:55:36 +0800

    lab3 modified

--- lab3.c
+++ lab3.c
@@ -6,7 +6,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <wait.h>
-#define MAXN 1024
+#include <errno.h>
+#define MAXN 4096
 int s1,s2;//信号灯
 int *buf,*ptr;
 union semun
@@ -20,7 +21,9 @@ void P(int semid){
     sem.sem_num= 0;
     sem.sem_op=-1;
     sem.sem_flg=0;
-    semop(semid,&sem,1);
+    if(semop(semid,&sem,1)==-1) {
+        printf("P semop error: %d \n",errno);
+        exit(EXIT_FAILURE);}
     return ;
 }
 void V(int semid){
@@ -28,71 +31,61 @@ void V(int semid){
 	sem.sem_num= 0;
 	sem.sem_flg= 0;
 	sem.sem_op= 1;
-	semop(semid,&sem,1);
+	if(semop(semid,&sem,1)==-1) puts("V semop error");
 	return;
 }
 void sem_init(int semid,int val){
 	union semun sem_val;
 	sem_val.val= val;
-	semctl(semid,0,SETVAL,sem_val);
+	if(semctl(semid,0,SETVAL,sem_val)<0) printf("semctl initial error\n");
 	return;
 }
-void writebuf(char *fname){
+void writebuf(char* fname,int s1,int s2){
     freopen(fname,"r",stdin);
-    freopen("mdzz","w",stdout);//debug
     int c;
     while(1){
         P(s1);
-        printf("ptr[1] is %d\n",ptr[1]);
-        while((ptr[0]+1)%MAXN!=ptr[1]){
-            c=getchar();
-            if(c==EOF) { V(s2); break;}
-            buf[ptr[0]]=c;
-            printf("%d\n",ptr[0]);
-            ptr[0]=(ptr[0]+1)%MAXN;
-        }
-        printf("one round\n");
+        c=getchar();
+        buf[ptr[0]]=c;
+        if(++ptr[0]==MAXN) ptr[0]=0;
         V(s2);
         if(c==EOF) break;
     }
-    ptr[2]=1;
     return ;
 }
-void readbuf(char* fname){
+void readbuf(char* fname,int s1,int s2){
     freopen(fname,"w",stdout);
     while(1){
         P(s2);
-        while((ptr[1]+1)%MAXN!=ptr[0]){
-            ptr[1]=(ptr[1]+1)%MAXN;
-            printf("%c",buf[ptr[1]]);
-        }
+        if(buf[ptr[1]]==EOF) break;
+        printf("%c",buf[ptr[1]]);
+        if(++ptr[1]==MAXN) ptr[1]=0;
         V(s1);
-        if(ptr[2]) break;
     }
-    return ;
+    return;
 }
 int main(int argc,char*argv[])
 {
-    int shmid1=shmget((key_t)3,sizeof(char)*MAXN,0666|IPC_CREAT);
-    int shmid2=shmget((key_t)4,sizeof(int)*3,0666|IPC_CREAT);
+    int shmid1=shmget((key_t)3,sizeof(int)*MAXN,0666|IPC_CREAT);
+    int shmid2=shmget((key_t)4,sizeof(int)*2,0666|IPC_CREAT);
     buf=(int*)shmat(shmid1,0,0);//share buf
-    ptr=(int*)shmat(shmid2,0,0);//ptr[0]->rptr,ptr[1]->wptr,ptr[2]->finish flag
-    ptr[0]=1;//initialize
+    ptr=(int*)shmat(shmid2,0,0);//ptr[0]->rptr,ptr[1]->wptr
+    ptr[0]=0;//initialize
     ptr[1]=0;
     ptr[2]=0;
-	int s1=semget(1,1,IPC_CREAT|0666);
-	int s2=semget(2,1,IPC_CREAT|0666);
-    sem_init(s1,1);
+	int s1=semget(10,1,IPC_CREAT|0666);
+	int s2=semget(20,1,IPC_CREAT|0666);
+    sem_init(s1,MAXN-4);
     sem_init(s2,0);
     pid_t p1,p2;p1=fork();
     if(p1==0){
-        writebuf(argv[1]);
+        writebuf(argv[1],s1,s2);
         exit(0);
     }
     else{
         p2=fork();
         if(p2==0){
-            readbuf(argv[2]);
+            readbuf(argv[2],s1,s2);
             exit(0);
         }
     }
@@ -100,5 +93,7 @@ int main(int argc,char*argv[])
     waitpid(p2,NULL,0);
     shmctl(shmid1,IPC_RMID,0);
     shmctl(shmid2,IPC_RMID,0);
+    semctl(s1,0,IPC_RMID);
+    semctl(s2,0,IPC_RMID);
     return 0;
 }
